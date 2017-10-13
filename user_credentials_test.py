cf3d91af1ee16078367214a1f164810fe25f5771
marynganga <marynganga.mw@gmail.com>
2017-10-13 12:40:36 +0300

    Create a test to save new account details.

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -17,7 +17,7 @@ class TestUser(unittest.TestCase):
 
 	def test__init__(self):
 		'''
-		Test to if check the initialization/creation of instances is properly done
+		Test to if check the initialization/creation of user instances is properly done
 		'''
 		self.assertEqual(self.new_user.first_name,'Mary')
 		self.assertEqual(self.new_user.last_name,'Ng\'ang\'a')
@@ -42,6 +42,23 @@ class TestCredentials(unittest.TestCase):
 		Function to create an account's credentials before each test
 		'''
 		self.new_account = Credential('Facebook','maryjoe','pswd100')
+
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of credential instances is properly done
+		'''
+		self.assertEqual(self.new_account.site_name,'Facebook')
+		self.assertEqual(self.new_account.account_name,'maryjoe')
+		self.assertEqual(self.new_account.password,'pswd100')
+
+	def test_save_credential(self):
+		'''
+		Test to check if the new users info is saved into the users list
+		'''
+		self.new_user.save_account()
+		self.twitter = Credential('Twitter','maryjoe','pswd100')
+		self.twitter.save_account()
+		self.assertEqual(len(Credential.credentials_list),2)
 
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
