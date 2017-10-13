472a7bcc38391c57e58588be49e10f90386e03bc
marynganga <marynganga.mw@gmail.com>
2017-10-13 11:20:33 +0300

    Create the Class User. 1. Create an __init__ method to lay out the properties a user object will have.

---
+++ user_credentials.py
@@ -0,0 +1,15 @@
+import pyperclip
+
+class User():
+	'''
+	Class to create user accounts and save their information
+	'''
+	def __init__(self,first_name,second_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
+
+		# instance variables
+		self.first_name = first_name
+		self.last_name = last_name
+		self.password = password
