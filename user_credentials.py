6e87b7493c5827ec5eb75e4cdc5c7f7fefe0bcb9
marynganga <marynganga.mw@gmail.com>
2017-10-13 14:17:11 +0300

    Create a class method to find a credential using the site_name

--- user_credentials.py
+++ user_credentials.py
@@ -53,4 +53,13 @@ class Credential:
 		'''
 		Class method to display the list of credentials saved
 		'''
-		return cls.credentials_list	
+		return cls.credentials_list
+		
+	@classmethod
+	def find_by_site_name(cls, site_name):
+		'''
+	    Method that takes in a site_name and returns a credential that matches that site_name.
+	    '''
+	    for credential in credentials_list:
+	    	if credential.site_name == site_name:
+	    		return credential
