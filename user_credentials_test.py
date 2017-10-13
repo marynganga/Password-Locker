a1e2d8395112f177a9c6c3cf5947fb99e76278ad
marynganga <marynganga.mw@gmail.com>
2017-10-13 12:11:25 +0300

    Add a test for saing user info

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -2,7 +2,7 @@ import unittest
 import pyperclip
 from user_credentials import User
 
-class TestUserCredentials(unittest.TestCase):
+class TestUser(unittest.TestCase):
 	'''
 	Test class that defines test cases for the contact class behaviours.
 
@@ -22,6 +22,13 @@ class TestUserCredentials(unittest.TestCase):
 		self.assertEqual(self.new_user.first_name,'Mary')
 		self.assertEqual(self.new_user.last_name,'Ng\'ang\'a')
 		self.assertEqual(self.new_user.password,'pswd100')
+
+	def test_save_user(self):
+		'''
+		Test to check if the new users info is saved into the users list
+		'''
+		self.new_user.save_user()
+		self.assertEqual(len(User.users_list),1)
 
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
